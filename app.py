37e99b60ffdfdcd7ba4c8f07c580b03be1a01085
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-12 16:30:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 import cv2
 import pytesseract
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 def extract_text_from_image(img_path):
     try:
@@ -17,12 +19,25 @@ def extract_text_from_image(img_path):
         print(f"Error extracting text from image: {e}")
         return None
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def show_image():
-    img_path = 'static/clipboard_image.png'
-
-    extracted_text = extract_text_from_image(img_path)
-    return render_template('index.html', img_path=img_path, text=extracted_text)
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return render_template('index.html', error='No image selected.')
+        
+        image_file = request.files['image']
+        if image_file.filename == '':
+            return render_template('index.html', error='No image selected.')
+        
+        # Save the uploaded image to the UPLOAD_FOLDER
+        filename = secure_filename(image_file.filename)
+        img_path = app.config['UPLOAD_FOLDER'] + '/' + filename
+        image_file.save(img_path)
+        
+        extracted_text = extract_text_from_image(img_path)
+        return render_template('index.html', img_path=img_path, text=extracted_text)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
