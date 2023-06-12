d0846c3a8e83b2456ee6f07c7c67aa831979361a
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-12 16:40:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
 import cv2
 import pytesseract
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
 
 def extract_text_from_image(img_path):
     try:
         img = cv2.imread(img_path)
         if img is None:
             raise ValueError("Failed to load image")
-        
+
         grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         text = pytesseract.image_to_string(grayscale_img)
         return text
@@ -22,22 +20,14 @@ def extract_text_from_image(img_path):
 @app.route('/', methods=['GET', 'POST'])
 def show_image():
     if request.method == 'POST':
-        if 'image' not in request.files:
-            return render_template('index.html', error='No image selected.')
-        
         image_file = request.files['image']
-        if image_file.filename == '':
-            return render_template('index.html', error='No image selected.')
-        
-        # Save the uploaded image to the UPLOAD_FOLDER
-        filename = secure_filename(image_file.filename)
-        img_path = app.config['UPLOAD_FOLDER'] + '/' + filename
+        img_path = 'static/' + image_file.filename
         image_file.save(img_path)
-        
+
         extracted_text = extract_text_from_image(img_path)
         return render_template('index.html', img_path=img_path, text=extracted_text)
-    else:
-        return render_template('index.html')
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
