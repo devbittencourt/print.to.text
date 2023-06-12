1f8c160b408c09dea83bda0dfa8b9801d7f04634
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-12 16:55:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-from PIL import ImageGrab, Image
+from PIL import Image
 import pytesseract
+import subprocess
 
 app = Flask(__name__)
-
-
 
 def extract_text_from_image(img_path):
     img = Image.open(img_path)
@@ -13,14 +12,14 @@ def extract_text_from_image(img_path):
 
 @app.route('/')
 def show_image():
-    img = ImageGrab.grabclipboard()
+    subprocess.run(["wl-paste", "--output", "png", "-o", "static/clipboard_image.png"])
     img_path = 'static/clipboard_image.png'
 
-    if img is not None:
-        img.save(img_path)
+    try:
+        img = Image.open(img_path)
         extracted_text = extract_text_from_image(img_path)
         return render_template('index.html', img_path=img_path, text=extracted_text)
-    else:
+    except IOError:
         return render_template('index.html', img_path=None, text=None)
 
 if __name__ == '__main__':
