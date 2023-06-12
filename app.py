61233da09f6831df7603a72fa09b4234f4dd3b50
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-12 16:17:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask import Flask, render_template
-from PIL import ImageGrab, Image
+import cv2
 import pytesseract
-import pyperclip
 
 app = Flask(__name__)
 
 def extract_text_from_image(img_path):
-    img = Image.open(img_path)
-    text = pytesseract.image_to_string(img)
+    img = cv2.imread(img_path)
+    grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    text = pytesseract.image_to_string(grayscale_img)
     return text
 
 @app.route('/')
 def show_image():
-    img = ImageGrab.grabclipboard()
     img_path = 'static/clipboard_image.png'
 
-    if img is not None:
-        img.save(img_path)
-        extracted_text = extract_text_from_image(img_path)
-        return render_template('index.html', img_path=img_path, text=extracted_text)
-    else:
-        return render_template('index.html', img_path=None, text=None)
+    extracted_text = extract_text_from_image(img_path)
+    return render_template('index.html', img_path=img_path, text=extracted_text)
 
 if __name__ == '__main__':
     app.run()
