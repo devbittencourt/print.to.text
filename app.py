5dad687c27768668c46423c9b0e145cf698a5f43
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-12 16:49:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,27 @@
-from flask import Flask, render_template, request
-import cv2
+from flask import Flask, render_template
+from PIL import ImageGrab, Image
 import pytesseract
 
 app = Flask(__name__)
 
+
+
 def extract_text_from_image(img_path):
-    try:
-        img = cv2.imread(img_path)
-        if img is None:
-            raise ValueError("Failed to load image")
+    img = Image.open(img_path)
+    text = pytesseract.image_to_string(img)
+    return text
 
-        grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        text = pytesseract.image_to_string(grayscale_img)
-        return text
-    except Exception as e:
-        print(f"Error extracting text from image: {e}")
-        return None
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def show_image():
-    if request.method == 'POST':
-        image_file = request.files['image']
-        img_path = 'static/' + image_file.filename
-        image_file.save(img_path)
+    img = ImageGrab.grabclipboard()
+    img_path = 'static/clipboard_image.png'
 
+    if img is not None:
+        img.save(img_path)
         extracted_text = extract_text_from_image(img_path)
         return render_template('index.html', img_path=img_path, text=extracted_text)
-
-    return render_template('index.html')
+    else:
+        return render_template('index.html', img_path=None, text=None)
 
 if __name__ == '__main__':
     app.run()
