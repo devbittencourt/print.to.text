75323b6f41f92d39b2752803f17eff2eef3d6b4b
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-13 11:30:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
-from flask import Flask, render_template
-from PIL import Image
+from flask import Flask, render_template, request
+from PIL import ImageGrab, Image
 import pytesseract
-import subprocess
+import base64
+import os
 
 app = Flask(__name__)
+
 
 def extract_text_from_image(img_path):
     img = Image.open(img_path)
     text = pytesseract.image_to_string(img)
     return text
 
-@app.route('/')
-def show_image():
-    subprocess.run(["wl-paste", "--output", "png", "-o", "static/clipboard_image.png"])
-    img_path = 'static/clipboard_image.png'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Lê a imagem enviada pelo usuário
+        image_data = request.form['image']
+        
+        # Verifica se é uma imagem
+        if image_data.startswith('data:image'):
+            # Remove o prefixo "data:image/png;base64," para obter os dados da imagem
+            image_data = image_data.replace('data:image/png;base64,', '')
 
-    try:
-        img = Image.open(img_path)
-        extracted_text = extract_text_from_image(img_path)
-        return render_template('index.html', img_path=img_path, text=extracted_text)
-    except IOError:
-        return render_template('index.html', img_path=None, text=None)
+            # Decodifica a string em base64 para obter os dados binários da imagem
+            image_bytes = base64.b64decode(image_data)
+
+            # Define o caminho de destino do arquivo
+            upload_folder = 'static'
+            filename = 'uploaded_image.png'
+            destination = os.path.join(upload_folder, filename)
+
+            # Salva a imagem no caminho de destino
+            with open(destination, 'wb') as f:
+                f.write(image_bytes)
+
+            # Extrai o texto da imagem
+            extracted_text = extract_text_from_image(destination)
+
+            # Retorna uma mensagem de sucesso e o texto extraído
+            return f'Imagem enviada com sucesso!\n\nTexto extraído:\n\n{extracted_text}'
+
+    # Renderiza o template HTML
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
