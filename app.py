78b71c946b9df4cf1bd9842af65955be1672b6ef
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-14 13:36:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 app = Flask(__name__)
 ocr_pipeline = pipeline.Pipeline()
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def process_image():
     image_base64 = request.form.get('image')
 
