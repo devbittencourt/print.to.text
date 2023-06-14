25139e09bb87e98a28e9bc5379762e5f537b90de
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-14 13:59:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from keras_ocr import pipeline
 import os
 import base64
@@ -10,24 +10,27 @@ os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 app = Flask(__name__)
 ocr_pipeline = pipeline.Pipeline()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def process_image():
-    image_file = request.files['image']
+    if request.method == 'POST':
+        image_file = request.files['image']
 
-    if image_file is None:
-        return jsonify({'error': 'Image data not provided'})
+        if image_file is None:
+            return jsonify({'error': 'Image data not provided'})
 
-    # Lê a imagem a partir do arquivo
-    image = Image.open(image_file)
+        # Lê a imagem a partir do arquivo
+        image = Image.open(image_file)
 
-    # Realiza a leitura da imagem usando OCR
-    result = ocr_pipeline.recognize([image])
+        # Realiza a leitura da imagem usando OCR
+        result = ocr_pipeline.recognize([image])
 
-    # Extrai o texto detectado
-    text = result[0]['text']
+        # Extrai o texto detectado
+        text = result[0]['text']
 
-    # Retorna o texto como resposta em formato JSON
-    return jsonify({'text': text})
+        # Retorna o texto como resposta em formato JSON
+        return jsonify({'text': text})
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
