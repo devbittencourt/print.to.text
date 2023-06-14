3fd724a422b32135d02f985e35749c987a22cb03
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-14 15:26:44 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from keras_ocr import pipeline
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def process_image():
+    if request.method == 'POST':
+        # Verifica se foi fornecido o arquivo de imagem
+        if 'image' not in request.files:
+            return jsonify({'error': 'Nenhum arquivo de imagem fornecido.'})
+
+        image = request.files['image']
+        
+        # Lê a imagem a partir do arquivo
+        image_data = image.read()
+
+        # Utiliza o Keras-OCR pipeline para realizar a leitura da imagem e extrair o texto
+        pipeline = pipeline.Pipeline()
+        result = pipeline.recognize(image_data)
+
+        # Retorna o texto extraído como resposta em formato JSON
+        return jsonify({'text': result})
+
+    elif request.method == 'GET':
+        # Retorna o conteúdo do arquivo 'index.html'
+        with open('index.html', 'r') as file:
+            return file.read()
+
+if __name__ == '__main__':
+    app.run()
