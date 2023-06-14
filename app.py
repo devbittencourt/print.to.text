8d12e6c8a561028f54104aec9faa4b9efee160c3
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-14 13:35:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
 from keras_ocr import pipeline
 import os
+import base64
+from PIL import Image
+import io
+
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
 app = Flask(__name__)
 ocr_pipeline = pipeline.Pipeline()
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def process_image():
     image_base64 = request.form.get('image')
 
+    # Decodifica a string base64 em uma matriz de imagem
+    image_data = base64.b64decode(image_base64)
+    image = Image.open(io.BytesIO(image_data))
+
     # Realiza a leitura da imagem usando OCR
-    result = ocr_pipeline.recognize([image_base64])
+    result = ocr_pipeline.recognize([image])
 
     # Extrai o texto detectado
     text = result[0]['text']
