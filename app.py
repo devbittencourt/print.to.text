a5af708355bbcc0bfc983012388efe917a83de2e
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2023-06-14 12:33:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,21 @@
-from flask import Flask, render_template, request
-from PIL import ImageGrab, Image
-import easyocr
-import base64
-import os
+from flask import Flask, request, jsonify
+from keras_ocr import pipeline
 
 app = Flask(__name__)
+ocr_pipeline = pipeline.Pipeline()
 
-def extract_text_from_image(img_path):
-    reader = easyocr.Reader(['en'])
-    img = Image.open(img_path)
-    results = reader.readtext(img)
-    return results
+@app.route('/', methods=['POST'])
+def process_image():
+    image_base64 = request.form.get('image')
 
+    # Realiza a leitura da imagem usando OCR
+    result = ocr_pipeline.recognize([image_base64])
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        # Lê a imagem enviada pelo usuário
-        image_data = request.form['image']
-        
-        # Verifica se é uma imagem
-        if image_data.startswith('data:image'):
-            # Remove o prefixo "data:image/png;base64," para obter os dados da imagem
-            image_data = image_data.replace('data:image/png;base64,', '')
+    # Extrai o texto detectado
+    text = result[0]['text']
 
-            # Decodifica a string em base64 para obter os dados binários da imagem
-            try:
-                image_bytes = base64.b64decode(image_data)
-            except Exception as e:
-                return f'Erro ao decodificar a imagem: {str(e)}'
-
-            # Define o caminho de destino do arquivo
-            upload_folder = 'static'
-            filename = 'uploaded_image.png'
-            destination = os.path.join(upload_folder, filename)
-
-            # Salva a imagem no caminho de destino
-            try:
-                with open(destination, 'wb') as f:
-                    f.write(image_bytes)
-            except Exception as e:
-                return f'Erro ao salvar a imagem: {str(e)}'
-
-            # Extrai o texto da imagem
-            try:
-                extracted_results = extract_text_from_image(destination)
-                extracted_text = '\n'.join([result[1] for result in extracted_results])
-            except Exception as e:
-                return f'Erro ao extrair texto da imagem: {str(e)}'
-
-            # Retorna uma mensagem de sucesso e o texto extraído
-            return f'Imagem enviada com sucesso!\n\nTexto extraído:\n\n{extracted_text}'
-
-    # Renderiza o template HTML
-    return render_template('index.html')
+    # Retorna o texto como resposta em formato JSON
+    return jsonify({'text': text})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run()
